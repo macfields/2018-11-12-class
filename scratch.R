e9231cafd4a378e472dbbf56a1e53f27e315a744
Maclaine Fields <maclainefields@Maclaines-MacBook-Pro.local>
2018-11-12 14:27:26 -0500

    working on function

--- scratch.R
+++ scratch.R
@@ -1,13 +1,17 @@
 library(tidyverse)
 
 download_link <- function(district, wave){
-  x <- paste("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/","elections-poll-", district, "-", wave, ".csv", sep = "")
   
-  return(x) #return something in the middle to check
+  district <- tolower(district)
+  
+  stopifnot(is.character(district))
+  
+  x <- paste("https://raw.githubusercontent.com/TheUpshot/2018-live-poll-results/master/data/","elections-poll-", district, "-", wave, ".csv", sep = "")
   
   y <- read_csv(x)
   
   invisible(y)
-  #puts all the three arguments together. However, its not currently in the environment.Need to Command + Enter whole function to add to environment.  
+  #puts all the three arguments together. However, its not currently in the environment.Need to Command + Enter whole function to add to environment.
+  
 }
 
